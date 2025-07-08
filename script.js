de1d51bfdaf78e47d3aeb3a2b9da51aa9e413b50
Jaydeep Nitin Gedam <jaydeep.gedam444@gmail.com>
2025-07-09 01:16:06 +0530

    updated script.js with sample middleware, routes and error handling.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Hello, World!");
+const express = require('express');
+const app = express();
+
+// Middleware to log the request URL
+app.use(function (req, res, next) {
+    console.log('Request URL:', req.url);
+    next();
+});
+
+// Route to handle GET requests to the root URL
+app.get('/', (req, res, next) => {
+    // res.send('Hello, World!');
+    return next(new Error('An error occurred!'));
+});
+
+// Middleware to handle errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(3000);
